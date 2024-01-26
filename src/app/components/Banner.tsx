fea64b66378c5ec0aab185c4ebccd5b571b9bbc1
simantar <simanta@hashtag-ca.com>
2024-01-26 10:45:05 +0530

    hero banner

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -11,16 +11,18 @@ export default function Banner() {
         layout="fill"
         objectFit="cover"
       />
-      <div></div>
-      <h1 className="absolute font-heroFont text-5xl mt-4 ml-2 lg:ml-28 md:mr-4 mr-0">
+      <h1 className="absolute font-heroFont text-5xl w-full top-0 mt-8 text-left ml-8 lg:ml-32 md:mr-4 mr-0 mb-4">
         daysgone
       </h1>
-      <div className="absolute w-full bottom-0 text-left ml-2 lg:ml-28 md:mr-4 mr-0">
-        <button className="text-white bg-yellow-500 px-10 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-100 cursor-pointer">
+      <div className="absolute w-full bottom-0 text-left ml-8 lg:ml-32 md:mr-4 mr-0 mb-4">
+        <button className="text-white bg-yellow-500 px-8 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-100 cursor-pointer">
           Try For Free
         </button>
         <p className="">Buy now for $40 only</p>
-        <p>40 of your friends are playing</p>
+        <p className="mt-4 text-sm flex items-center gap-4">
+          <div className="my-2 w-2 h-2 bg-green-500 drop-shadow-smshadow-lg rounded-full" />
+          <span>40 of your friends are playing</span>
+        </p>
       </div>
     </div>
   );
